e855d07136b425019761db856e6c51c35812e6c8
yhhno <516061849@qq.com>
2015-11-23 13:35:06 +0800

    first

---
+++ src/Mvc6Samples/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc6Samples.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
